0d880d841ee2c5be449e6dcd616e830cd5d6da3e
Kartik <kartikvatsa@hotmail.com>
2022-06-23 12:33:20 +0530

    v1.0.2

---
+++ c-class-templates.cpp
@@ -0,0 +1,59 @@
+/*
+ * TODO: CHECK THIS AGAIN
+ * One test case did'nt passed
+ * Test case 3
+ * Reason: Time limit exceeded
+ */
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <cassert>
+using namespace std;
+
+/*Write the class AddElements here*/
+template <typename T>
+class AddElements {
+	private:
+		T e1;
+
+	public:
+		AddElements(const T &e) : e1(e) {}
+
+		T add(T e) {
+			return (T) this->e1 + e;
+		}
+
+		string concatenate(string e) {
+			return (string) this->e1 + e;
+		}
+};
+
+int main () {
+  int n,i;
+  cin >> n;
+  for(i=0;i<n;i++) {
+    string type;
+    cin >> type;
+    if(type=="float") {
+        double element1,element2;
+        cin >> element1 >> element2;
+        AddElements<double> myfloat (element1);
+        cout << myfloat.add(element2) << endl;
+    }
+    else if(type == "int") {
+        int element1, element2;
+        cin >> element1 >> element2;
+        AddElements<int> myint (element1);
+        cout << myint.add(element2) << endl;
+    }
+    else if(type == "string") {
+        string element1, element2;
+        cin >> element1 >> element2;
+        AddElements<string> mystring (element1);
+        cout << mystring.concatenate(element2) << endl;
+    }
+  }
+  return 0;
+}
